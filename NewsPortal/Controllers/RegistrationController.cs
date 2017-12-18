bbb524b3db2b1cf53eed12d81f22943092b994ce
Dmitiy Shmaliuk <dimon4ik1977a@gmail.com>
2017-12-18 17:49:05 +0200

    Добавьте файлы проекта.

---
+++ NewsPortal/Controllers/RegistrationController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NewsPortal.Models;
+
+namespace NewsPortal.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(RegisterModel model)
+        {
+            User user = new User() { Email = model.Email, Login = model.Login, Password = model.Password };
+
+            var session = NHibernateHelper.GetCurrentSession();
+            try
+            {
+                if (user == null)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                using (var tx = session.BeginTransaction())
+                {
+                    var list = session.QueryOver<User>().List();
+
+                    //var criteria = session.CreateCriteria<User>();
+                    //var list = criteria.List<User>();
+
+                    foreach (var item in list)
+                    {
+                        if (user.Email == item.Email)
+                        {
+                            return RedirectToAction("Index", "Home");
+                        }
+                    }
+                    if (ModelState.IsValid)
+                    {
+                        session.Save(user);
+                        tx.Commit();
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+            finally
+            {
+                NHibernateHelper.CloseSession();
+            }
+            return RedirectToAction("Index", "Authorization");
+        }
+    }
+}
---
+++ NewsPortal/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewsPortal.Models
+{
+    public class User
+    {
+        public virtual int Id { set; get; }
+        public virtual string Email { set; get; }
+        public virtual string Login { set; get; }
+        public virtual string Password { set; get; }
+
+        //public string FirstName { set; get; }
+        //public string LastName { set; get; }
+        //public string PathToAvatar { set; get; }
+        //public DateTime DateOfBirth { set; get; }
+        //public DateTime RegistrationDate { set; get; }
+
+        // Виды юзеров будут добавлены позже
+        // public Status Status { set; get; }
+    }
+}
